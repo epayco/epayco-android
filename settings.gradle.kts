a3a4833f1bb0a3cc6538f718baac8de7f4928e1d
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2025-03-13 17:58:16 -0500

    feat:se actualiza gradle version}

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,11 +18,6 @@ dependencyResolutionManagement {
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
     }
-    versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
-    }
 }
 
 rootProject.name = "Sdk epayco"
