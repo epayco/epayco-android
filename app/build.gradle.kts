27a7973fca906608e083050531f9177855ca3b64
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2025-03-13 17:49:11 -0500

    feat:se actualiza gradle version}

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
