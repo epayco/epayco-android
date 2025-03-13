b1e3d31ec33251b6ad97412013a8d7979ec28b52
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2025-03-13 17:56:39 -0500

    feat:se actualiza gradle version}

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("org.jetbrains.kotlin.compose") version "1.0.0" apply false
     id("maven-publish")
 }
 
