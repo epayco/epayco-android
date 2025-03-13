5e10ef7cfda8d5d34d35be9ef5e1a0d1ce85b41d
RicardoSaldarriagaPayco <ricardo.saldarriaga@epayco.com>
2025-03-13 17:37:53 -0500

    feat:se actualiza gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,20 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id("maven-publish")
+}
+
+// Configuración del plugin maven-publish
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri("file://${buildDir}/repo")
+        }
+    }
 }
